f2e7d9a2fbdbac618cf64a6fb2d5becf6d6c4e13
buffetng <ulaitorcodes@gmail.com>
2024-02-13 20:45:16 +0100

    first commit

---
+++ ecotrack/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ecotrack.users.models import User
+from ecotrack.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
